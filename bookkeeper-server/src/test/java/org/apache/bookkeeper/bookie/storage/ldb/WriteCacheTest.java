d5a2886535469f9b2310b52e402a50d71fa092e0
gronz <gianronz12@gmail.com>
2024-10-05 14:35:54 +0200

    coverage jacoco

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/storage/ldb/WriteCacheTest.java
@@ -4,15 +4,13 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.buffer.UnpooledByteBufAllocator;
 import org.junit.Assert;
-import org.junit.experimental.runners.Enclosed;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
-
-import static org.mockito.internal.matchers.text.ValuePrinter.print;
 
 @RunWith(Enclosed.class)
 public class WriteCacheTest {
@@ -22,16 +20,17 @@ public class WriteCacheTest {
         private final long ledgerId;
         private final long entryId;
         private final ByteBuf expected;
-        private final Class<? extends Exception> expectedException;
+        private final boolean expectedException;
 
         @Parameterized.Parameters
         public static Collection<Object[]> getParameters() {
             return Arrays.asList(new Object[][]{
-                    {1L,1L,Unpooled.wrappedBuffer("testValue".getBytes()),null},
-                    {-1L,-1L,null,Exception.class}
+                    {-1L,-1L,null,true},
+                    {0L,0L,null,false},//cerco di prendere da uno non scritto forse separa 0 da non valido
+                    {1L,1L,Unpooled.wrappedBuffer("value".getBytes()),false}
             });
         }
-        public TestGet(long ledgerId, long entryId, ByteBuf expected, Class<? extends Exception> expectedException){
+        public TestGet(long ledgerId, long entryId, ByteBuf expected, boolean expectedException){
             this.ledgerId = ledgerId;
             this.entryId = entryId;
             this.expected = expected;
@@ -41,26 +40,23 @@ public class WriteCacheTest {
         public void testGet() {
 
             try {
-                WriteCache writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 100 * 1024 * 1024); // 100MB
-                ByteBuf value = Unpooled.wrappedBuffer("testValue".getBytes());
+                WriteCache writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 9,16);
+
                 if(expected!=null){
-                    writeCache.put(ledgerId, entryId, value);
+                    writeCache.put(ledgerId, entryId, Unpooled.wrappedBuffer("value".getBytes()));
                 }
                 // Recupera il valore con la chiave specifica
                 ByteBuf result = writeCache.get(ledgerId, entryId);
 
                 // Verifica che il valore sia corretto
                 //assertNotNull(result);
-                if(expectedException != null){
-                    Assert.assertEquals(value.compareTo(result), 0);
-                }
-                else{
+                Assert.assertEquals(expected,result);
 
-                }
             }
             catch (Exception e){
-                System.out.print("lanciata eccezione");
-
+                if (!expectedException){
+                    Assert.fail();
+                }
             }
     }
     }
@@ -78,12 +74,22 @@ public class WriteCacheTest {
             for(int i = 0; i<=5;i++ ){
                 nonValid = nonValid.concat("nonValid");
             }
-            print(nonValid.getBytes().length);
             return Arrays.asList(new Object[][]{
+
                     {-1L,-1L,null,false,true},
-                    {0L,0L,Unpooled.wrappedBuffer(nonValid.getBytes()),false,false},
+                    {0L,0L,Unpooled.wrappedBuffer(nonValid.getBytes()),false,false},//superata lasize
+
+
                     {1L,1L,Unpooled.wrappedBuffer("value".getBytes()),true,false},
-                    {1L,1L,Unpooled.wrappedBuffer("".getBytes()),true,false}
+
+
+                    {1L,1L,Unpooled.wrappedBuffer("".getBytes()),true,false},//stringa vuota
+                    //aggiunto dopo jacoco
+                    {1L,1L,Unpooled.wrappedBuffer("valuevalue".getBytes()),false,false},//segment-offset<size
+
+
+                    {2L,1L,Unpooled.wrappedBuffer("value".getBytes()),true,false},//provo a scrivere un entry prima dell ultimw
+                    {3L,9L,Unpooled.wrappedBuffer("value".getBytes()),true,false}//provo a scrivere un entry con id che già c'e
             });
         }
         public TestPut(long ledgerId, long entryId, ByteBuf entry, boolean expected, boolean expectedException){
@@ -97,7 +103,18 @@ public class WriteCacheTest {
         public void testPut() {
 
             try {
-                WriteCache writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 9,16);
+                WriteCache writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 15,8);
+                if(ledgerId == 2L){
+                    writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 1024,8);
+                    writeCache.put(ledgerId, 2L, entry);
+                }
+                if(ledgerId == 3L){
+                    writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 1024,8);
+                    //writeCache.put(ledgerId, 8L, entry);
+                    writeCache.put(ledgerId, 1L, entry);
+                    writeCache.put(ledgerId,2L,Unpooled.wrappedBuffer("value3".getBytes()));
+
+                }
                 Assert.assertEquals(expected,writeCache.put(ledgerId, entryId, entry));
                 if(expected){
                     ByteBuf result = writeCache.get(ledgerId, entryId);
@@ -115,5 +132,119 @@ public class WriteCacheTest {
         }
 
 
+    }
+
+    @RunWith(Parameterized.class)
+    public static class TestGetLastEntry{
+            private final long ledgerId;
+            private final ByteBuf expected;
+            private final boolean expectedException;
+
+            @Parameterized.Parameters
+            public static Collection<Object[]> getParameters() {
+                return Arrays.asList(new Object[][]{
+                        {-1L,null,true},
+                        {0L,null, false},//non ci è stato scritto
+                        {1L,Unpooled.wrappedBuffer("value".getBytes()), false},
+                });
+            }
+            public TestGetLastEntry(long ledgerId, ByteBuf expected, boolean expectedException){
+                this.ledgerId = ledgerId;
+                this.expected = expected;
+                this.expectedException = expectedException;
+            }
+            @Test
+            public void testGetLastEntry() {
+
+                try {
+                    WriteCache writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 9,16);
+                    writeCache.put(1L, 1L, Unpooled.wrappedBuffer("value".getBytes()));
+                    ByteBuf result = writeCache.getLastEntry(ledgerId);
+                    Assert.assertEquals(expected,result);
+
+
+                } catch (Exception e) {
+                    if (!expectedException){
+                        Assert.fail();
+                    }
+                    //System.out.print("eccezzione lanciata");
+
+                }
+            }
+
+    }
+    @RunWith(Parameterized.class)
+    public static class TestDeleteLedger {
+        private final long ledgerId;
+        private final boolean expected;
+        private final boolean expectedException;
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> getParameters() {
+            return Arrays.asList(new Object[][]{
+                    {-1L, false, true},
+                    {0L, true, false},
+                    {1L, true, false},
+            });
+        }
+
+        public TestDeleteLedger(long ledgerId, boolean expected, boolean expectedException) {
+            this.ledgerId = ledgerId;
+            this.expected = expected;
+            this.expectedException = expectedException;
+        }
+
+        @Test
+        public void testGetLastEntry() {
+
+            try {
+                WriteCache writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 9, 16);
+                writeCache.put(ledgerId, 1L, Unpooled.wrappedBuffer("value".getBytes()));
+                writeCache.deleteLedger(ledgerId);
+            } catch (Exception e) {
+                if (!expectedException) {
+                    Assert.fail();
+                }
+                System.out.print("eccezzione lanciata");
+
+            }
+        }
+    }
+    @RunWith(Parameterized.class)
+    public static class TestHasEntry{
+        private final long ledgerId;
+        private final long entryId;
+        private final boolean expected;
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> getParameters() {
+            return Arrays.asList(new Object[][]{
+                    {-1L,-1L,false},
+                    {0L,0L,false},// non c'era entry
+                    {1L,1L,true},//c'era lentry
+
+            });
+        }
+        public TestHasEntry(long ledgerId, long entryId, boolean expected){
+            this.ledgerId = ledgerId;
+            this.entryId = entryId;
+            this.expected = expected;
+        }
+        @Test
+        public void testHasEntry() {
+
+            try {
+                WriteCache writeCache = new WriteCache(UnpooledByteBufAllocator.DEFAULT, 9,16);
+
+
+                writeCache.put(1L, 1L, Unpooled.wrappedBuffer("value".getBytes()));
+
+                boolean result = writeCache.hasEntry(ledgerId, entryId);
+                Assert.assertEquals(expected,result);
+
+            }
+            catch (Exception e){
+            }
+        }
     }
 }
